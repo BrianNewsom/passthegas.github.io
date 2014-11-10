2393ca7b0f45f1b5590278a2a103ebdfcd0208a9
Brian Newsom <brian.newsom@colorado.edu>
2014-11-09 21:58:18 -0700

    simplify autocomplete js

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,3 +1,4 @@
+// Commented code left to allow for integration with map
 function initializeACStart() {
   var start = /** @type {HTMLInputElement} */(
       document.getElementById('start'));
@@ -5,27 +6,27 @@ function initializeACStart() {
   var autocomplete = new google.maps.places.Autocomplete(start);
   //autocomplete.setType('address');
 
-  var infowindow = new google.maps.InfoWindow();
+  // var infowindow = new google.maps.InfoWindow();
 
-  google.maps.event.addListener(autocomplete, 'place_changed', function() {
-    infowindow.close();
-    var place = autocomplete.getPlace();
+  // google.maps.event.addListener(autocomplete, 'place_changed', function() {
+  //   infowindow.close();
+  //   var place = autocomplete.getPlace();
 
-    var address = '';
-    if (place.address_components) {
-      address = [
-        (place.address_components[0] && place.address_components[0].short_name || ''),
-        (place.address_components[1] && place.address_components[1].short_name || ''),
-        (place.address_components[2] && place.address_components[2].short_name || '')
-      ].join(' ');
-    }
+  //   var address = '';
+  //   if (place.address_components) {
+  //     address = [
+  //       (place.address_components[0] && place.address_components[0].short_name || ''),
+  //       (place.address_components[1] && place.address_components[1].short_name || ''),
+  //       (place.address_components[2] && place.address_components[2].short_name || '')
+  //     ].join(' ');
+  //   }
 
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    infowindow.open(map, marker);
-  });
+  //   infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+  //   infowindow.open(map, marker);
+  // });
 
 }
-
+// Commented code left to allow for integration with map
 function initializeACEnd() {
   var end = /** @type {HTMLInputElement} */(
       document.getElementById('end'));
@@ -33,24 +34,24 @@ function initializeACEnd() {
   var autocomplete = new google.maps.places.Autocomplete(end);
   //autocomplete.setType('address');
 
-  var infowindow = new google.maps.InfoWindow();
+  // var infowindow = new google.maps.InfoWindow();
 
-  google.maps.event.addListener(autocomplete, 'place_changed', function() {
-    infowindow.close();
-    var place = autocomplete.getPlace();
+  // google.maps.event.addListener(autocomplete, 'place_changed', function() {
+  //   infowindow.close();
+  //   var place = autocomplete.getPlace();
 
-    var address = '';
-    if (place.address_components) {
-      address = [
-        (place.address_components[0] && place.address_components[0].short_name || ''),
-        (place.address_components[1] && place.address_components[1].short_name || ''),
-        (place.address_components[2] && place.address_components[2].short_name || '')
-      ].join(' ');
-    }
+  //   var address = '';
+  //   if (place.address_components) {
+  //     address = [
+  //       (place.address_components[0] && place.address_components[0].short_name || ''),
+  //       (place.address_components[1] && place.address_components[1].short_name || ''),
+  //       (place.address_components[2] && place.address_components[2].short_name || '')
+  //     ].join(' ');
+  //   }
 
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    infowindow.open(map, marker);
-  });
+  //   infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+  //   infowindow.open(map, marker);
+  // });
 
 }
 
